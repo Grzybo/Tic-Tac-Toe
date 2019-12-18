20bccb4fb98d4974742b3c850c06ffda23a244ed
Bartek Grzybowski <b.grzybowski.20@gmail.com>
2019-12-18 23:41:20 +0100

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 144);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 157);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(12, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 144);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(172, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 144);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Transparent;
+            this.button5.Location = new System.Drawing.Point(172, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 157);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.Transparent;
+            this.button6.Location = new System.Drawing.Point(172, 325);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 144);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.Transparent;
+            this.button7.Location = new System.Drawing.Point(341, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(145, 144);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.Color.Transparent;
+            this.button8.Location = new System.Drawing.Point(341, 162);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(145, 157);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.Color.Transparent;
+            this.button9.Location = new System.Drawing.Point(332, 325);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(154, 144);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.Location = new System.Drawing.Point(494, 51);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(138, 66);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(546, 201);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Easy";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(513, 364);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 87);
+            this.textBox1.TabIndex = 11;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(546, 228);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 24);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Hard";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(644, 487);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe by Grzybo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Hello! You play as X. Let's start!";
+
+
+        }
+
+        bool end = false;
+
+        Image X = (Image)TicTacToe.Properties.Resources.ResourceManager.GetObject("kolko_krzyzyk_x");
+        Image O = (Image)TicTacToe.Properties.Resources.ResourceManager.GetObject("kolko_krzyzyk_o"); 
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button button;
+            button = sender as Button;
+
+            if (button.Image == null)
+            {
+                button.Image = X;
+
+                CheckX(X);
+                if(end == false)
+                {
+                    Circle();
+                }
+                CheckX(O);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Circle()
+        {
+            Random rand = new Random();
+
+            int x = 0;
+
+            bool flag = false;
+
+            while (flag == false)
+            {
+                x++;
+
+                if( x == 10)
+                {
+                    flag = true;
+                }
+
+                switch (x)
+                {
+                    case 1:
+                        if (button1.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 2:
+                        if (button2.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 3:
+                        if (button3.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 4:
+                        if (button4.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 5:
+                        if (button5.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 6:
+                        if (button6.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 7:
+                        if (button7.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 8:
+                        if (button8.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                    case 9:
+                        if (button9.Image == null)
+                        {
+                            flag = true;
+                        }
+                        break;
+                } 
+                
+            }
+
+            
+
+                switch (x)
+                {
+                    case 1:
+                        button1.Image = O;
+                        break;
+                    case 2:
+                        button2.Image = O;
+                        break;
+                    case 3:
+                        button3.Image = O;
+                        break;
+                    case 4:
+                        button4.Image = O;
+                        break;
+                    case 5:
+                        button5.Image = O;
+                        break;
+                    case 6:
+                        button6.Image = O;
+                        break;
+                    case 7:
+                        button7.Image = O;
+                        break;
+                    case 8:
+                        button8.Image = O;
+                        break;
+                    case 9:
+                        button9.Image = O;
+                        break;
+                }
+            
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = false;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Image = null;
+            button2.Image = null;
+            button3.Image = null;
+            button4.Image = null;
+            button5.Image = null;
+            button6.Image = null;
+            button7.Image = null;
+            button8.Image = null;
+            button9.Image = null;
+
+            textBox1.Text = "Hello! You play as X. Let's start!";
+
+            ButtonsUnlock();
+
+            end = false;
+        }
+
+        private void CheckX(Image image)
+        {
+            string win = "You WIN!!!";
+            string lose = "You LOSE!!!";
+            string tekst = null;
+
+            if(image == X)
+            {
+                tekst = win;
+            }
+            else
+            {
+                tekst = lose;
+            }
+
+            //pionowo
+            if ((button1.Image == image) && (button2.Image == image) && (button3.Image == image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            if ((button4.Image == image) && (button5.Image == image) && (button6.Image == image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            if ((button7.Image == image) && (button8.Image == image) && (button9.Image == image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            //poziomo
+            if ((button1.Image == image) && (button4.Image == image) && (button7.Image == image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            if ((button2.Image ==image) && (button5.Image == image) && (button8.Image == image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            if ((button3.Image == image) && (button5.Image == image) && (button9.Image ==image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            //ukos
+            if ((button1.Image ==image) && (button5.Image ==image) && (button9.Image ==image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+            if ((button7.Image ==image) && (button5.Image ==image) && (button3.Image ==image))
+            {
+                textBox1.Text = tekst;
+                ButtonsLock();
+                end = true;
+            }
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+        }
+        private void ButtonsLock() // blokuje przyciski
+        {
+
+            if( button1.Image == null)
+            {
+                button1.Enabled = false;
+            }
+            if (button2.Image == null)
+            {
+                button2.Enabled = false;
+            }
+
+            if (button3.Image == null)
+            {
+                button3.Enabled = false;
+            }
+
+            if (button4.Image == null)
+            {
+                button4.Enabled = false;
+            }
+
+            if (button5.Image == null)
+            {
+                button5.Enabled = false;
+            }
+            if (button6.Image == null)
+            {
+                button6.Enabled = false;
+            }
+            if (button7.Image == null)
+            {
+                button7.Enabled = false;
+            }
+            if (button8.Image == null)
+            {
+                button8.Enabled = false;
+            }
+            if (button9.Image == null)
+            {
+                button9.Enabled = false;
+            }
+
+        }
+        private void ButtonsUnlock() // odblokowywuje przyciski
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true; 
+        }
+    }
+}
